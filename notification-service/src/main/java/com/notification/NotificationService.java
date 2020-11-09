31c890d37b0a37839b94e15da62faced8dddf2ac
vignesh-ragavan <vigneshr.vtalent@gmail.com>
2020-11-08 23:33:15 -0800

    added user reg and authentication service

---
+++ notification-service/src/main/java/com/notification/NotificationService.java
@@ -0,0 +1,8 @@
+
+package com.notification;
+
+
+public interface NotificationService {
+
+	
+}
---
+++ user-authn-service/src/main/java/com/authn/utils/AuthnConstants.java
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+package com.authn.utils;
+
+
+public interface AuthnConstants {
+
+	String IDP_AUTHN_TYPE="IDPLogin";
+	String DB_AUTHN_TYPE="DBLogin";
+}
